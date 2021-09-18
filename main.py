09ef5dfe409901338f2f3a89de38791448bcc880
Aneiongit <86514605+Aneiongit@users.noreply.github.com>
2021-09-18 16:18:16 +0100

    Fix sketch info

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-divisive = []
-for num in range(0, 100):
-    if num % 5 == 0:
-        divisive.append(num)
-cube = []
-for number in divisive:
-    cube.append(number**3)
-print(divisive)
-print(cube)
+print("'Hiszpańska inkwizycja' to najlepszy skecz grupy Monty Pythona")
